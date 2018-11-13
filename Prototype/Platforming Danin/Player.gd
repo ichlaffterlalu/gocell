0c4a4528cada70137453663c9215cee55c2837de
Daninsky <arceusgummi@gmail.com>
2018-11-13 13:39:44 +0700

    Movement and boost mechanism. Max boost speed and stamina not implemented

--- Prototype/Platforming Danin/Player.gd
+++ Prototype/Platforming Danin/Player.gd
@@ -2,81 +2,55 @@ extends KinematicBody2D
 
 const UP = Vector2(0,-1)
 const GRAVITY = 20
-const SPEED = 200
-const TOP_SPEED = 1000
+const ACCEL = 10
+const NORMAL_SPEED = 200
+const MAX_SPEED = 400
 const JUMP_HEIGHT = -600
-const STAMINA = 0
 
 export var id = 0
-var motion = Vector2()
-var ACCELERATION_RIGHT = 200
-var ACCELERATION_LEFT = -200
 
+var current_max_speed = MAX_SPEED
+var motion = Vector2()
+var moving = false
+var boost = true
+var stamina = 5
+var accel_right = 0
+var accel_left = 0
 
 
 func _physics_process(delta):
 	motion.y += GRAVITY
-	
-	
-	if Input.is_action_pressed("ui_right_%s" % id):
-		ACCELERATION_LEFT = -SPEED
-		
-		if (STAMINA >= 100000):
-			ACCELERATION_RIGHT = SPEED
-			STAMINA = 0
-			
-		motion.x = ACCELERATION_RIGHT
-		STAMINA += motion.x
-
-	elif Input.is_action_pressed("ui_left_%s" % id):
-		ACCELERATION_RIGHT = SPEED
-		
-		if (STAMINA >= 100000):
-			ACCELERATION_LEFT = -SPEED
-			STAMINA = 0
-			
-		motion.x = ACCELERATION_LEFT
-		STAMINA += -motion.x
-	
-	elif Input.is_action_just_released("ui_right_%s" % id):
-		ACCELERATION_LEFT = -SPEED
-		
-		if (ACCELERATION_RIGHT + SPEED < TOP_SPEED):
-			ACCELERATION_RIGHT += SPEED
-		else :
-			ACCELERATION_RIGHT = TOP_SPEED
-		
-		if (STAMINA >= 100000):
-			ACCELERATION_RIGHT = SPEED
-			STAMINA = 0
-				
-		motion.x = ACCELERATION_RIGHT
-		STAMINA += motion.x
-	
-	elif Input.is_action_just_released("ui_left_%s" % id):
-		ACCELERATION_RIGHT = SPEED
-		
-		if (ACCELERATION_LEFT - SPEED > TOP_SPEED):
-			ACCELERATION_LEFT -= SPEED
+	if Input.is_action_just_pressed("ui_right_%s" % id):
+		if motion.x > 0:
+			boost = true
+			motion.x += 1200
+			current_max_speed += 100
+		if boost:
+			print ("RIGHT BOOST. Current maximum speed = ",current_max_speed)
+	elif Input.is_action_just_pressed("ui_left_%s" % id):
+		if motion.x < 0:
+			boost = true
+			motion.x -= 1200
+		if boost:
+			print ("LEFT BOOST. Current maximum speed = ",current_max_speed)
+	elif Input.is_action_pressed("ui_right_%s" % id):
+		if boost:
+			motion.x = lerp(motion.x + ACCEL, current_max_speed,0.45)
 		else:
-			ACCELERATION_LEFT = -TOP_SPEED
-			
-		if (STAMINA >= 100000):
-			ACCELERATION_LEFT = -SPEED
-			STAMINA = 0
-			
-		motion.x = ACCELERATION_LEFT
-		STAMINA += -motion.x
-	
-			
+			motion.x = min(motion.x + ACCEL, current_max_speed)
+	elif Input.is_action_pressed("ui_left_%s" % id):
+		if boost:
+			motion.x = lerp(motion.x - ACCEL, -MAX_SPEED,0.45)
+		else:
+			motion.x = max(motion.x - ACCEL, -MAX_SPEED)
 	else:
-		motion.x = 0
-		
+		motion.x = lerp(motion.x, 0, 0.1)
+		if motion.x == 0:
+			boost = false
+			
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up_%s" % id):
 			motion.y = JUMP_HEIGHT
-			
 	
 	motion = move_and_slide(motion,UP)
 	pass
-	

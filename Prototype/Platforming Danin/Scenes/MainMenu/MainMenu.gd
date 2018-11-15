19493de0143ee27a463a04487f61193608d0fc5c
Ichlasul Affan <ichlaffterlalu@gmail.com>
2018-11-16 01:35:19 +0700

    Now button works

--- Prototype/Platforming Danin/Scenes/MainMenu/MainMenu.gd
+++ Prototype/Platforming Danin/Scenes/MainMenu/MainMenu.gd
@@ -8,13 +8,18 @@ var scene_path_to_load
 func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
+	$Fade.show()
+	$Fade.fade_out()
 	for button in $MainPage/MarginContainer/HBoxContainer/LeftSide/MenuButtons.get_children():
 		button.connect("pressed", self, "_on_Button_pressed", [button.scene_to_load])
 
 func _on_Button_pressed(scene_to_load):
 	scene_path_to_load = scene_to_load
-	$FadeIn.show()
-	$FadeIn.fade_in()
+	$Fade.show()
+	$Fade.fade_in()
 
-func _on_FadeIn_fade_finished():
+func _on_Fade_fade_in_finished():
 	get_tree().change_scene(scene_path_to_load)
+
+func _on_Fade_fade_out_finished():
+	$Fade.hide()

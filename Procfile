f273a62815c573634e9ffb581fc6b5ac38b40d98
Agas Yanpratma <agas.yanpratama@gmail.com>
2018-12-01 09:29:40 +0700

    presskit

---
+++ Procfile
@@ -0,0 +1,2 @@
+migrate: bash deployment.sh
+web: gunicorn templatePPW.wsgi --log-file -
---
+++ templatePPW/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for templatePPW project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'templatePPW.settings')
+
+application = get_wsgi_application()

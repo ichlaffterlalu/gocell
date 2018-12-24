bd9c6abaf4fc864ca54afac91d9d3a21b2e1acdb
72ridwan <rachmat.ridwan72@gmail.com>
2018-12-24 07:10:53 +0700

    Fix volume issues

--- Main/Scripts/Settings.gd
+++ Main/Scripts/Settings.gd
@@ -2,13 +2,14 @@ extends Control
 
 signal need_restart()
 
-onready var int_bus_master = AudioServer.get_bus_index("Master")
-onready var int_bus_music = AudioServer.get_bus_index("Music")
-onready var int_bus_sfx = AudioServer.get_bus_index("SFX")
+var BUS_MASTER = AudioServer.get_bus_index("Master")
+var BUS_MUSIC = AudioServer.get_bus_index("Music")
+var BUS_SFX = AudioServer.get_bus_index("SFX")
 onready var sfx_control = $TabContainer/Sounds/MarginContainer/Container/Sliders/SFX
 onready var master_control = $TabContainer/Sounds/MarginContainer/Container/Sliders/Master
 onready var music_control = $TabContainer/Sounds/MarginContainer/Container/Sliders/Music
 onready var sound_demo = $AudioSettingSFX
+var init_sound_slider = false
 
 var snd_last_play = OS.get_ticks_msec()
 
@@ -26,10 +27,27 @@ func _ready():
 #	# Update game logic here.
 #	pass
 
+func _set_bus_vol_percent(bus_idx, vol_percent):
+	AudioServer.set_bus_volume_db(bus_idx, 10*log(vol_percent/100))
+
+func _get_vol_percent(bus_idx):
+	return exp(AudioServer.get_bus_volume_db(bus_idx)/10) * 100
+
+func _init_sound_controllers():
+	sfx_control.value = _get_vol_percent(BUS_SFX)
+	master_control.value = _get_vol_percent(BUS_MASTER)
+	music_control.value = _get_vol_percent(BUS_MUSIC)
+	print("Audio Controls")
+	print(sfx_control.value)
+	print(master_control.value)
+	print(music_control.value)
+
 func toggle(status):
 	if not status and self.is_visible_in_tree():
 		$TabContainer.fade_out()
+		init_sound_slider = false
 	elif status and not self.is_visible_in_tree():
+		_init_sound_controllers()
 		$TabContainer.fade_in()
 		self.show()
 
@@ -37,25 +55,21 @@ func _on_TabContainer_fade_out_finished():
 	$TabContainer.hide() # replace with function body
 	self.hide()
 
-func set_bus_to_vol_percent(bus_idx, vol_percent):
-	AudioServer.set_bus_volume_db(bus_idx, 10*log(vol_percent/100))
-
-
 func _on_Master_sound_value_changed(value):
-	set_bus_to_vol_percent(int_bus_master, value)
-
+	_set_bus_vol_percent(BUS_MASTER, value)
 
 func _on_SFX_value_changed(value):
-	var snd_now = OS.get_ticks_msec()
-	if (snd_now - snd_last_play > 200):
-		sound_demo.play()
-		snd_last_play = snd_now
-	set_bus_to_vol_percent(int_bus_sfx, value)
-
+	if init_sound_slider:
+		var snd_now = OS.get_ticks_msec()
+		if (snd_now - snd_last_play > 200):
+			sound_demo.play()
+			snd_last_play = snd_now
+		_set_bus_vol_percent(BUS_SFX, value)
+	else:
+		init_sound_slider = true
 
 func _on_Music_value_changed(value):
-	set_bus_to_vol_percent(int_bus_music, value)
-
+	_set_bus_vol_percent(BUS_MUSIC, value)
 
 func _on_ResetLeaderboards_pressed():
 	var dialog = ConfirmationDialog.new()
